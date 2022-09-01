4720870356bc86933a9bc7dbd785a313e4aa292e
brteo <matteo@meblabs.com>
2022-09-01 12:23:51 +0200

    ci: load dev env if exist

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+
+if (process.env.ENV === 'dev' && fs.existsSync(path.resolve(__dirname, '.env.dev'))) {
+	dotenv.config({ path: path.resolve(__dirname, '.env.dev') });
+} else {
+	dotenv.config();
+}
 
 const app = require('./app');
 require('./db/connect');
